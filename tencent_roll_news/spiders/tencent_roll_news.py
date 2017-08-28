fdec17e79e9ad122c4977dccaf7ef64a2fcde8fe
jfzhang95 <jfzhang@whu.edu.cn>
2017-08-28 14:09:35 +0800

    增加获取新闻评论数功能

--- tencent_roll_news/spiders/tencent_roll_news.py
+++ tencent_roll_news/spiders/tencent_roll_news.py
@@ -5,16 +5,12 @@
 @date:   2017-08-28
 """
 
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
 from scrapy.selector import Selector
 import json
 import re
 from scrapy import Request, Spider
 import time
 import datetime
-import requests
 from tencent_roll_news.items import TencentRollNewsItem
 
 def ListCombiner(lst):
@@ -35,7 +31,7 @@ class TencentNewsSpider(Spider):
     date_time = datetime.datetime.now().strftime('%Y-%m-%d')
     time_stamp = int(round(time.time()*1000))
     def start_requests(self):
-        yield Request(self.list_url.format(date='2017-06-07', page='1', time_stamp=str(self.time_stamp)), self.parse_list)
+        yield Request(self.list_url.format(date=self.date_time, page='1', time_stamp=str(self.time_stamp)), self.parse_list)
 
     def parse_list(self, response):
         results = json.loads(response.text[9:-1])
@@ -45,20 +41,20 @@ class TencentNewsSpider(Spider):
             title = element['title']
             column = element['column']
             url = element['url']
-            yield Request(url, self.parse_news, meta={'column':column,
-                                                      'url':url,
-                                                      'title':title,
-                                                      'time':time_
-                                                      })
+            if column != u'图片':
+                yield Request(url, self.parse_news, meta={'column':column,
+                                                          'url':url,
+                                                          'title':title,
+                                                          'time':time_
+                                                         })
         list_page = results['data']['page']
         list_count = results['data']['count']
         if list_page < list_count:
             time_stamp = int(round(time.time() * 1000))
-            yield Request(self.list_url.format(date='2017-06-07', page=str(list_page+1), time_stamp=str(time_stamp)), self.parse_list)
+            yield Request(self.list_url.format(date=self.date_time, page=str(list_page+1), time_stamp=str(time_stamp)), self.parse_list)
 
     def parse_news(self, response):
         sel = Selector(response)
-        item = TencentRollNewsItem()
 
         url = response.meta['url']
         title = response.meta['title']
@@ -72,42 +68,34 @@ class TencentNewsSpider(Spider):
         newsId = pattern.group(3)
         contents = ListCombiner(sel.xpath('//p/text()').extract()[:-3])
 
-        item['source'] = source
-        item['time'] = time_
-        item['date'] = date
-        item['contents'] = contents
-        item['title'] = title
-        item['url'] = url
-        item['newsId'] = newsId
-        item['column'] = column
-        return item
 
-        # if sel.xpath('//*[@id="Main-Article-QQ"]/div/div[1]/div[2]/script[2]/text()'):
-        #     cmt = sel.xpath('//*[@id="Main-Article-QQ"]/div/div[1]/div[2]/script[2]/text()').extract()[0]
-        #     if re.findall(r'cmt_id = (\d*);', cmt):
-        #         cmt_id = re.findall(r'cmt_id = (\d*);', cmt)[0]
-        #         comment_url = 'http://coral.qq.com/article/{}/comment?commentid=0&reqnum=1&tag=&callback=mainComment&_=1389623278900'.format(cmt_id)
-        #         yield Request(comment_url, self.parse_comment, meta={'source': source,
-        #                                                              'date': date,
-        #                                                              'newsId': newsId,
-        #                                                              'url': url,
-        #                                                              'title': title,
-        #                                                              'contents': contents,
-        #                                                              'time': time_,
-        #                                                              'column': column,
-        #                                                              })
+        try:
+            cmt = sel.xpath('//*[@id="Main-Article-QQ"]/div/div[1]/div[2]/script[2]/text()').extract()[0]
+            cmt_id = re.findall(r'cmt_id = (\d*);', cmt)[0]
+        except:
+            item = TencentRollNewsItem()
+            item['source'] = source
+            item['time'] = time_
+            item['date'] = date
+            item['contents'] = contents
+            item['title'] = title
+            item['url'] = url
+            item['newsId'] = newsId
+            item['comments'] = 0
+            item['column'] = column
+            return item
 
-        # else:
-        #     item['source'] = source
-        #     item['time'] = time_
-        #     item['date'] = date
-        #     item['contents'] = contents
-        #     item['title'] = title
-        #     item['url'] = url
-        #     item['newsId'] = newsId
-        #     item['comments'] = 0
-        #     item['column'] = column
-        #     return item
+        comment_url = 'http://coral.qq.com/article/{}/comment?commentid=0&reqnum=1&tag=&callback=mainComment&_=1389623278900'.format(cmt_id)
+        yield Request(comment_url, self.parse_comment, dont_filter=True, meta={'source': source,
+                                                                               'date': date,
+                                                                               'newsId': newsId,
+                                                                               'url': url,
+                                                                               'title': title,
+                                                                               'contents': contents,
+                                                                               'time': time_,
+                                                                               'column': column,
+                                                                               })
+
 
 
 
@@ -117,7 +105,6 @@ class TencentNewsSpider(Spider):
         else:
             comments = 0
         item = TencentRollNewsItem()
-        print(response.text)
         item['source'] = response.meta['source']
         item['time'] = response.meta['time']
         item['date'] = response.meta['date']
